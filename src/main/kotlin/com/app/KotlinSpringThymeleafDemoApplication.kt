c887d8ca954d2acb720ddd6bc3a2df4d44646850
Valmir Junior <valmirknasel@gmail.com>
2020-10-17 20:14:46 -0400

    Initial Commit
    Signed-off-by: Valmir Junior <valmirknasel@gmail.com>

---
+++ src/main/kotlin/com/app/KotlinSpringThymeleafDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringThymeleafDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringThymeleafDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/KotlinSpringThymeleafDemoApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.app
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = arrayOf(KotlinSpringThymeleafDemoApplication::class),webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class KotlinSpringThymeleafDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        val test = 1
+        assertEquals(test, 1)
+        println("ok")
+    }
+
+}
